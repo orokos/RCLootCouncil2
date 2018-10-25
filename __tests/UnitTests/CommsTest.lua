6fd9842955aaa23e3293100dfe6f3f0077b633f3
evil-morfar <evil_morfar@hotmail.com>
2018-10-25 16:51:02 +0200

    More comms test

--- __tests/UnitTests/CommsTest.lua
+++ __tests/UnitTests/CommsTest.lua
@@ -40,7 +40,13 @@ TestBasics = {
    end,
    TestSend = function (args)
       rc:Send("group", "test1", "test data")
+		WoWAPI_FireUpdate(GetTime()+100)
    end,
+
+	TestSend2 = function()
+		rc.Comms:Send{command = "command", target = "group", data = "test data"}
+		WoWAPI_FireUpdate(GetTime()+100)
+	end,
 }
 
 TestReceiving = {
@@ -50,8 +56,10 @@ TestReceiving = {
       self.received = false
    end,
    TearDown = function(self)
+		WoWAPI_FireUpdate(GetTime()+100)
       lu.assertTrue(self.received)
    end,
+
    TestReceive = function(args)
       local toSend = "some test data"
       rc.Comms:RegisterCommand(rc, rc.Comms.Prefixes.MAIN, "test",
@@ -63,8 +71,8 @@ TestReceiving = {
          lu.assertEquals(sender, "Sender")
       end)
       rc:Send("group", "test", toSend)
-      WoWAPI_FireUpdate(GetTime()+100)
    end,
+
    TestReceive2 = function(args)
       local toSend = {bigger = "data", "structure"}
       rc.Comms:RegisterCommand(rc, rc.Comms.Prefixes.MAIN, "test2",
@@ -76,10 +84,65 @@ TestReceiving = {
          lu.assertEquals(sender, "Sender")
       end)
       rc:Send("group", "test2", toSend)
-      WoWAPI_FireUpdate(GetTime()+100)
    end,
 
-   TestOtherPrefix = function (args)
+	TestBulkRegisterCommand = function(args)
+		local toSend = "some test data"
+		rc.Comms:BulkRegisterCommand(rc, rc.Comms.Prefixes.MAIN,
+			{
+				test10 = function(rc, command, data, distri, sender)
+		         args.received1 = true
+		         lu.assertEquals(command, "test10")
+		         lu.assertEquals(unpack(data), toSend)
+		         lu.assertEquals(distri, "RAID")
+		         lu.assertEquals(sender, "Sender")
+		      end,
+				test11 = function(rc, command, data, distri, sender)
+					lu.assertTrue(args.received1)
+		         args.received = true
+		         lu.assertEquals(command, "test11")
+		         lu.assertEquals(unpack(data), toSend)
+		         lu.assertEquals(distri, "RAID")
+		         lu.assertEquals(sender, "Sender")
+		      end
+			})
+		rc:Send("group", "test10", toSend)
+		rc:Send("group", "test11", toSend)
+	end,
+
+	TestxPersistency = function(args) -- x to run last
+		-- Check if the commands registered in the previous tests still works
+		local toSend = "some test data"
+		rc:Send("group", "test10", toSend)
+		rc:Send("group", "test11", toSend)
+		WoWAPI_FireUpdate(GetTime()+100)
+		lu.assertTrue(args.received)
+		args.received = false
+		rc:Send("group", "test", toSend)
+		WoWAPI_FireUpdate(GetTime()+100)
+		lu.assertTrue(args.received)
+		args.received = false
+		toSend = {bigger = "data", "structure"}
+		rc:Send("group", "test2", toSend)
+	end,
+
+	TestReceiveCommsSend = function (args)
+		-- Should be received with TestReceive
+		rc.Comms:Send{command = "test", data = "some test data"}
+	end
+}
+
+TestOtherPrefixes = {
+	Setup = function(self)
+      _G.IsInRaidVal = true
+      self.received = false
+   end,
+   TearDown = function(self)
+		WoWAPI_FireUpdate(GetTime()+100)
+      lu.assertTrue(self.received)
+   end,
+
+	TestVersionPrefix = function (args)
       rc.Send = rc.Comms:Register(rc, rc.Comms.Prefixes.VERSION)
       local toSend = {version = "3.0.0", tVersion = "Alpha.1"}
       rc.Comms:RegisterCommand(rc, rc.Comms.Prefixes.VERSION, "v",
@@ -90,9 +153,74 @@ TestReceiving = {
          lu.assertEquals(distri, "RAID")
          lu.assertEquals(sender, "Sender")
       end)
-      rc:Send("group", "test", toSend)
-      WoWAPI_FireUpdate(GetTime()+100)
+      rc:Send("group", "v", toSend)
+   end,
+
+	TestNewPrefix = function(args)
+		rc.Send = rc.Comms:Register(rc, "newPrefix")
+		local toSend = {data = "this is", some = "data"}
+		rc.Comms:RegisterCommand(rc, "newPrefix", "test",
+			function(rc, command, data, distri, sender)
+				args.received = true
+         lu.assertEquals(command, "test")
+         lu.assertEquals(unpack(data), toSend)
+         lu.assertEquals(distri, "RAID")
+         lu.assertEquals(sender, "Sender")
+			end)
+		rc:Send("group", "test", toSend)
+	end,
+
+	TestXCommsSend = function (args)
+		rc.Comms:Send{prefix = "newPrefix", data = {data = "this is", some = "data"}, command = "test"}
+	end
+}
+
+TestGuildComms = {
+	Setup = function(self)
+		rc.Send = rc.Comms:Register(rc, rc.Comms.Prefixes.VERSION)
+      self.received = false
+   end,
+   TearDown = function(self)
+		WoWAPI_FireUpdate(GetTime()+100)
+      lu.assertTrue(self.received)
+   end,
+	TestGuildComm = function (args)
+		local toSend = {version = "3.0.0", tVersion = "Alpha.1"}
+      rc.Comms:RegisterCommand(rc, rc.Comms.Prefixes.VERSION, "ver",
+      function(rc, command, data, distri, sender)
+         args.received = true
+         lu.assertEquals(command, "ver")
+         lu.assertEquals(unpack(data), toSend)
+         lu.assertEquals(distri, "GUILD")
+         lu.assertEquals(sender, "Sender")
+      end)
+      rc:Send("guild", "ver", toSend)
    end
+}
+
+TestPartyComms = {
+	Setup = function(self)
+		_G.IsInRaidVal = false
+		_G.IsInGroupVal = true
+		self.Send = rc.Comms:Register(rc, "party")
+      self.received = false
+   end,
+   TearDown = function(self)
+		WoWAPI_FireUpdate(GetTime()+100)
+      lu.assertTrue(self.received)
+   end,
+	TestPartyComm1 = function (self)
+		local toSend = "some party data"
+		rc.Comms:RegisterCommand(rc, "party", "test",
+		function(rc, command, data, distri, sender)
+			self.received = true
+	      lu.assertEquals(command, "test")
+	      lu.assertEquals(unpack(data), toSend)
+	      lu.assertEquals(distri, "PARTY")
+	      lu.assertEquals(sender, "Sender")
+		end)
+		self:Send("group", "test", toSend)
+	end
 }
 
 os.exit(lu.LuaUnit.run("-v"))
